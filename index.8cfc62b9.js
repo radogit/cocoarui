27fc775a162916c5ae1f078dd6b464c7efa883c5
rado@dukalski.com <rado@dukalski.com>
2025-03-17 13:03:27 +0100

    Updates

--- index.8cfc62b9.js
+++ index.8cfc62b9.js
@@ -24560,9 +24560,38 @@ function setupUI() {
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"8RTi2":[function(require,module,exports,__globalThis) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
-parcelHelpers.export(exports, "spawnNodes", ()=>spawnNodes);
-function spawnNodes() {
+parcelHelpers.export(exports, "addNewNode", ()=>addNewNode);
+function addNewNode() {
     console.log('spawning!');
+    // 1) The new node data
+    const newNode = {
+        id: "F",
+        x: Math.random() * 200 - 100,
+        y: Math.random() * 200 - 100,
+        color: "pink",
+        radius: 20,
+        isFixed: false,
+        significance: 10,
+        hotspots: []
+    };
+    // 2) Insert into the simulation
+    const oldNodes = simulation.nodes();
+    const newNodes = [
+        ...oldNodes,
+        newNode
+    ];
+    simulation.nodes(newNodes);
+    // 3) Re-bind to DOM
+    const nodeGroup = container.selectAll(".node-group").data(newNodes, (d)=>d.id).join((enter)=>{
+        const g = enter.append("g").attr("class", "node-group").call(d3.drag().on("start", dragStart).on("drag", dragging).on("end", dragEnd));
+        g.append("circle").attr("r", (d)=>d.radius).attr("fill", (d)=>d.color);
+        // ...
+        // ID label, coord label, etc., if you want
+        // ...
+        return g;
+    }, (update)=>update, (exit)=>exit.remove());
+    // 4) Re-heat
+    simulation.alpha(1).restart();
 }
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"kNCsT":[function(require,module,exports,__globalThis) {
