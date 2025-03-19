f7a3026fbf37a6a67a5bdd54d3769fcffa7d43cb
rado@dukalski.com <rado@dukalski.com>
2025-03-19 19:16:57 +0100

    safari iOS viewport height fix, another.

--- js/drawing.js
+++ js/drawing.js
@@ -5,7 +5,7 @@ import { collisionMargin } from "./data.js";
 export function createSvgAndContainer() {
   const width  = window.innerWidth;
   const height = window.visualViewport ? window.visualViewport.height : window.innerHeight;
-  if(window.visualViewport !== window.innerHeight) {console.log("user appears to be on mobile. good luck.", "pink");}
+  if(window.visualViewport !== window.innerHeight) {console.log("user appears to be on mobile. good luck.", "pink"); console.log(height, "pink"); console.log(window.visualViewport.height, "pink"); console.log(window.innerHeight, "pink");}
   const svg = d3.select("body")
     .append("svg")
     .attr("xmlns","http://www.w3.org/2000/svg")
