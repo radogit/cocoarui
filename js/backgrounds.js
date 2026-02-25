aebaf3a5dbcd9f80ea6a176b79c64db7193d0464
rado@dukalski.com <rado@dukalski.com>
2026-02-25 17:09:44 +0100

    replaced big PNG background with compressed JPG equivalents

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -5,10 +5,10 @@ export const imagePaths = [
     { name: 'backgroundBottomBase',   label: 'Bottom base',    width: 180, height: 90, x: -90, y: 18,   fileWidth: 1173, fileHeight: 341, opacity: 1, align: 'xMidYMax meet', url: require('./../img/background-bottom-base.png') },
     { name: 'backgroundTopUphill',    label: 'Top uphill',     width: 180, height: 90, x: -90, y: -44,  fileWidth: 1173, fileHeight: 468, opacity: 1, align: 'xMidYMin meet', url: require('./../img/background-top-uphill.jpg') },
     { name: 'backgroundTopDownhill',  label: 'Top downhill',   width: 180, height: 90, x: -90, y: -44,  fileWidth: 1173, fileHeight: 468, opacity: 1, align: 'xMidYMin meet', url: require('./../img/background-top-downhill.jpg') },
-    { name: 'backgroundVRDownhill',   label: 'VR downhill',    width: 360, height: 180, x: -180, y: -90,  fileWidth: 3840, fileHeight: 1920, opacity: 0.4, align: 'xMidYMin meet', url: require('./../img/screenshot_descent1_2to1.png') }, // replace url with VR asset when available
-    { name: 'backgroundVRUphill',     label: 'VR uphill',      width: 360, height: 180, x: -180, y: -90,  fileWidth: 3840, fileHeight: 1920, opacity: 0.4, align: 'xMidYMin meet', url: require('./../img/screenshot_ascent1_2to1.png') },   // replace url with VR asset when available
-    // { name: 'backgroundVRDownhill',   label: 'VR downhill',    width: 180, height: 124, x: -90, y: -34,  fileWidth: 1920, fileHeight: 1323, opacity: 0.4, align: 'xMidYMin meet', url: require('./../img/screenshot_descent1_2to1_+2-withnogrid.png') }, // replace url with VR asset when available
-    // { name: 'backgroundVRUphill',     label: 'VR uphill',      width: 180, height: 124, x: -90, y: -34,  fileWidth: 1920, fileHeight: 1323, opacity: 0.4, align: 'xMidYMin meet', url: require('./../img/screenshot_ascent1_2to1_+2-withnogrid.png') },   // replace url with VR asset when available
+    { name: 'backgroundVRDownhill',   label: 'VR downhill',    width: 360, height: 180, x: -180, y: -90,  fileWidth: 3840, fileHeight: 1920, opacity: 0.4, align: 'xMidYMin meet', url: require('./../img/screenshot_descent1_2to1.jpg') }, 
+    { name: 'backgroundVRUphill',     label: 'VR uphill',      width: 360, height: 180, x: -180, y: -90,  fileWidth: 3840, fileHeight: 1920, opacity: 0.4, align: 'xMidYMin meet', url: require('./../img/screenshot_ascent1_2to1.jpg') },   
+    // { name: 'backgroundVRDownhill',   label: 'VR downhill',    width: 180, height: 124, x: -90, y: -34,  fileWidth: 1920, fileHeight: 1323, opacity: 0.4, align: 'xMidYMin meet', url: require('./../img/screenshot_descent1_2to1_+2-withnogrid.png') }, 
+    // { name: 'backgroundVRUphill',     label: 'VR uphill',      width: 180, height: 124, x: -90, y: -34,  fileWidth: 1920, fileHeight: 1323, opacity: 0.4, align: 'xMidYMin meet', url: require('./../img/screenshot_ascent1_2to1_+2-withnogrid.png') },
 ];
 
 // User chooses one preset; each preset shows a set of images by name.
